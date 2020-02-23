05a98e95856546f6882a4f51eeec4a2165fb3bb3
Jakub Filipek <jakubflpk@gmail.com>
2020-02-22 16:32:22 -0800

    Updated demo to new use new funtion.

--- demo_trucaser.py
+++ demo_trucaser.py
@@ -1,25 +1,7 @@
-from truecase import load_truecaser, load_truecase_dataset, evaluate
-import pickle as pkl
+from datasets.brown.brown import Brown
+from truecase.external_utils import predict_truecasing
 
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-
-# Get device for gpu support
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-# Data is expected to be a file of one sentence per line.
-# Look for datasets/wiki/test.txt for an example
-dataset = load_truecase_dataset('datasets/wiki/test.txt')
-data_loader = DataLoader(dataset, batch_size=100)
-
-model = load_truecaser().to(device)
-criterion = nn.CrossEntropyLoss()
-
-loss_on_dataset, y_pred, y_truth = evaluate(model, criterion, data_loader)
-
-# You can use y_pred/y_truth to get statistics about data
-from sklearn.metrics import accuracy_score, f1_score
-print(f'Loss: {loss_on_dataset}')
-print('Accuracy: {:4f}'.format(accuracy_score(y_truth, y_pred)))
-print('F1 Score: {:4f}'.format(f1_score(y_truth, y_pred)))
+dataset = Brown()
+dataset = dataset.load_data_lowercase()
+dataset_sentences = dataset[0]
+truecased_senteces = predict_truecasing(dataset_sentences)
